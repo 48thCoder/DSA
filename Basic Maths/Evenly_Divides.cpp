c697d20692761b40c832cc28bfd0c77a4a827f01
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-06-24 23:18:36 +0530

    Document complexity analysis in evenlyDivides function
    Added comments to document time and space complexity.

---
+++ Basic Maths/Evenly_Divides.cpp
@@ -0,0 +1,22 @@
+// Given a positive integer n, count the number of digits in n that divide n evenly (i.e., without leaving a remainder). Return the total number of such digits.
+
+class Solution {
+public:
+    int evenlyDivides(int n) {
+        int num = n;
+        int count = 0;
+
+        while (num > 0) {
+            int digit = num % 10;
+            if (digit != 0 && n % digit == 0) {
+                count++;
+            }
+            num /= 10;
+        }
+
+        return count;
+    }
+};
+
+// Time Complexity: O(d) where, d is the no. of digits
+// Space Complexity: O(1)
