cdd9252ba839573ad58c0d1d3b35cf840c36310a
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-06-24 22:19:29 +0530

    Refactor extract_digits function into a class method

--- Basic Maths/extract_digits.cpp
+++ Basic Maths/extract_digits.cpp
@@ -1,29 +1,21 @@
 // Given an integer n, print each of its digits starting from the last digit and moving towards the first digit. Separate the digits with a space.
 
-#include <bits/stdc++.h>
-using namespace std;
+class Solution {
+public:
+    void extractDigits(int n) {
+        n = abs(n);
 
-int main()
-{
-    int n;
-    cin >> n;
-    n = abs(n);
+        if (n == 0) {
+            cout << 0;
+            return;
+        }
 
-    if (n == 0)
-    {
-        cout << 0;
-        return 0;
+        while (n > 0) {
+            cout << n % 10 << " ";
+            n /= 10;
+        }
     }
-
-    while (n > 0)
-    {
-        int digit = n % 10;
-        cout << digit << " ";
-        n /= 10;
-    }
-
-    return 0;
-}
+};
 
 // Time Complexity: O(d) where, d is the no. of digits
 // Space Complexity: O(1)
