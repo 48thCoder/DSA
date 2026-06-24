dcc92228dba257f1bdcea3c46f87a1a5f29d1087
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-06-24 22:35:38 +0530

    Add digit counting solutions in Count_Digits.cpp
    Implemented a brute force and an optimal solution for counting digits in a non-negative integer.

---
+++ Basic Maths/Count_Digits.cpp
@@ -0,0 +1,33 @@
+// Given a non-negative integer n, return the total number of digits present in n.
+
+// BRUTE FORCE APPROACH
+class Solution {
+public:
+    int countDigit(int n) {
+        int count = 0;
+        if(n == 0) {
+            return 1;
+        }
+        while(n > 0) {
+            count++;
+            n /= 10;
+        }
+        return count;
+    }
+};
+// Time Complexity: O(d) where, d is the no. of digits
+// Space Complexity: O(1)
+
+// OPTIMAL SOLUTION
+class Solution {
+public:
+    int countDigit(int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return (int)(log10(n) + 1);
+    }
+};
+// Time Complexity: O(1)
+// Space Complexity: O(1)
