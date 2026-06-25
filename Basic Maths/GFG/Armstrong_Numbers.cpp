44c37d223e82934e6ad2dce95b18a0e5537ba7d1
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-06-26 00:21:56 +0530

    Fix comment formatting for time complexity

--- Basic Maths/GFG/Armstrong_Numbers.cpp
+++ Basic Maths/GFG/Armstrong_Numbers.cpp
@@ -14,5 +14,5 @@ class Solution {
 	}
 };
 
-// Time Complexity: O(d), where d -> No. of digits ~ O(log10(n) + 1)
+// Time Complexity: O(d), where d -> No. of digits ~ O(log10(n))
 // Space Complexity: O(1)
