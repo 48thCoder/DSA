176d35875add9e766026c7a7a585939d40e450cc
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-07-23 16:10:47 +0530

    Improve largestPrimeFactor implementation
    Refactored the largestPrimeFactor function to use a better approach for finding the largest prime factor of n.

--- Basic Maths/GFG/Largest_prime_factor.cpp
+++ Basic Maths/GFG/Largest_prime_factor.cpp
@@ -1,5 +1,6 @@
 // Given a number n, your task is to find the largest prime factor of n.
 
+// NAIVE APPROACH
 class Solution {
 	public:
 	int largestPrimeFactor(int n) {
@@ -13,6 +14,25 @@ class Solution {
 		return maxPrime;
 	}
 };
+// Time Complexity: O(√n)
+// Space Complexity: O(1)
 
+// BETTER APPROACH
+class Solution {
+	public:
+	int largestPrimeFactor(int n) {
+		int maxPrime = n;
+		for (int i = 2; i * i <= n; i++) {
+			while (n % i == 0) {
+				maxPrime = i;
+				n = n / i;
+			}
+		}
+		if (n > 1) {
+			maxPrime = n;
+		}
+		return maxPrime;
+	}
+};
 // Time Complexity: O(√n)
 // Space Complexity: O(1)
