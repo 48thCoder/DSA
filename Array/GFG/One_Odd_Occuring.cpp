d10a99fff8e90e424070588c3f736d3be39fd9a6
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-09-24 01:08:03 +0530

    Add solution for finding odd occurring number in array
    Implemented multiple approaches to find the odd occurring number in an array, including brute force, sorting, hash map, and optimal XOR method.

---
+++ Array/GFG/One_Odd_Occuring.cpp
@@ -0,0 +1,80 @@
+// Given an array of arr[] positive integers where all numbers occur even number of times except one number which occurs odd number of times. Return that number.
+
+// BRUTE FORCE APPROACH -> TLE
+class Solution {
+	public:
+	int getOddOccurrence(vector<int>& arr) {
+		int n = arr.size();
+		
+		for (int i = 0; i < n; i++) {
+			int count = 0;
+			
+			for (int j = 0; j < n; j++) {
+				if (arr[j] == arr[i]) {
+					count++;
+				}
+			}
+			
+			if (count % 2 == 1) {
+				return arr[i];
+			}
+		}
+		return - 1;
+	}
+};
+// Time Complexity: O(n²)
+// Space Complexity: O(1)
+
+// BETTER APPROACH (Sorting)
+class Solution {
+	public:
+	int getOddOccurrence(vector<int>& arr) {
+		int n = arr.size();
+		
+		sort(arr.begin(), arr.end());
+		for (int i = 0; i < n - 1; i++) {
+			if (arr[i] != arr[i + 1]) {
+				return arr[i];
+			}
+			i++;
+		}
+		return arr[n - 1];
+	}
+};
+// Time Complexity: O(n log n) + O(n) ~ O(n log n)
+// Space Complexity: O(1)
+
+// USING HASH MAP
+class Solution {
+	public:
+	int getOddOccurrence(vector<int>& arr) {
+		unordered_map<int, int> freq;
+		for (int x : arr) {
+			freq[x]++;
+		}
+		
+		for (auto x : freq) {
+			if (x.second % 2 == 1) {
+				return x.first;
+			}
+		}
+		return - 1;
+	}
+};
+// Time Complexity: O(n) + O(n) ~ O(n + n) ~ O(n)
+// Space Complexity: O(n)
+
+// OPTIMAL SOLUTION (Using XOR)
+class Solution {
+	public:
+	int getOddOccurrence(vector<int>& arr) {
+		int n = arr.size();
+		int x = 0;
+		for (int i = 0; i < n; i++) {
+			x ^= arr[i];
+		}
+		return x;
+	}
+};
+// Time Complexity: O(n)
+// Space Complexity: O(1)
