45aaffa6d04b41a0d62644e9ccdc3679e60f827c
Vaibhav Gupta <guptavaibhav4248@gmail.com>
2026-09-24 17:00:15 +0530

    Modify space complexity comment in Missing_Number.cpp
    Updated space complexity comment to reflect possible O(n) usage.

--- Array/LeetCode/268_Missing_Number.cpp
+++ Array/LeetCode/268_Missing_Number.cpp
@@ -61,7 +61,7 @@ public:
     }
 };
 // Time Complexity: O(n log n) + O(n) ~ O(n log n)
-// Space Complexity: O(1)
+// Space Complexity: O(1) or O(n)
 
 // USING MATHEMATICAL FORMULA
 class Solution {
